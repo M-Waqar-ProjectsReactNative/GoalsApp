c05d1266fcd70e7ebfa390f076cd828ae553708b
muhammad Waqar <muhammadwaqar552@gmail.com>
2022-01-15 19:20:36 +0400

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  Button,
+  FlatList,
+  Modal,
+} from "react-native";
 
 export default function App() {
+  const [goals, setGoals] = useState("");
+  const [isAddMode, setIsAddMode] = useState(false);
+  const [goalList, setGoalList] = useState([]);
+
+  const inputChange = (text) => {
+    setGoals(text);
+  };
+
+  const addGoal = () => {
+    setGoalList((prevgoal) => [
+      { id: Math.random().toString(), goal: goals },
+      ...prevgoal,
+    ]);
+    setIsAddMode(false);
+    setGoals("");
+  };
+
+  const cancleGoal = () => {
+    setGoals("");
+    setIsAddMode(false);
+  };
+
+  const deleteGoal = (goalId) => {
+    setGoalList((prevList) => {
+      return prevList.filter((item) => item.id !== goalId);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add New Task" onPress={() => setIsAddMode(true)} />
+      <Modal visible={isAddMode} animationType="slide">
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Add Goals"
+            style={styles.textInput}
+            onChangeText={inputChange}
+            value={goals}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button title="ADD" onPress={addGoal} />
+            </View>
+            <View style={styles.button}>
+              <Button title="CANCLE" onPress={cancleGoal} />
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <FlatList
+        data={goalList}
+        renderItem={(itemData) => (
+          <TouchableOpacity onPress={() => deleteGoal(itemData.item.id)}>
+            <View style={styles.listItem}>
+              <Text>{itemData.item.goal}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+      {/* <ScrollView>
+        {goalList.map((goal) => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
+    padding: 40,
+  },
+  inputContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  buttonContainer: {
+    width: "80%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  button: {
+    width: "45%",
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "#ccc",
   },
 });
